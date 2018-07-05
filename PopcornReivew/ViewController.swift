4560c652a9b7a8e3d889339f2f7711f8f4d32dc9
mattmega4 <mattmega4@gmail.com>
2018-07-04 22:59:16 -0700

    Initial Commit

---
+++ PopcornReivew/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PopcornReivew
+//
+//  Created by Matthew Howes Singleton on 7/4/18.
+//  Copyright © 2018 Matthew Howes Singleton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
